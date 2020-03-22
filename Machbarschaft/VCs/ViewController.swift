7ca6fd747fffb9bfbcfb845886fa73402de7e8b4
Linus Geffarth <linus@geffarth.de>
2020-03-22 10:18:31 +0100

    created project

---
+++ Machbarschaft/VCs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Machbarschaft
+//
+//  Created by Linus Geffarth on 22.03.20.
+//  Copyright © 2020 Linus Geffarth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
