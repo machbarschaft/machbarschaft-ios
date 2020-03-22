138ab868f3004d6571e63fc254684f4f1857ce1e
Linus Geffarth <linus@geffarth.de>
2020-03-22 14:57:54 +0100

    added map overlay vc

--- Machbarschaft/VCs/MapOverlayViewController.swift
+++ Machbarschaft/VCs/MapOverlayViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class MapOverlayViewController: UIViewController {
     
+    @IBOutlet weak var urgencyButton: UIButton!
+    @IBOutlet weak var urgencyButtonIcon: UIImageView!
+    
+    @IBOutlet weak var closenessButton: UIButton!
+    @IBOutlet weak var closenessButtonIcon: UIImageView!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let highlightedColor = UIColor(named: "Link")
+    let defaultColor = UIColor(named: "Text")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    @IBAction func setSorting(_ button: UIButton) {
+        let isSortingByUrgency = button == urgencyButton
+        
+        urgencyButton.isHighlighted = isSortingByUrgency
+        urgencyButtonIcon.tintColor = isSortingByUrgency ? highlightedColor : defaultColor
+        closenessButton.isHighlighted = !isSortingByUrgency
+        closenessButtonIcon.tintColor = isSortingByUrgency ? defaultColor : highlightedColor
     }
 }
--- Machbarschaft/VCs/MapViewController.swift
+++ Machbarschaft/VCs/MapViewController.swift
@@ -14,15 +14,14 @@ class MapViewController: UIViewController {
     
     @IBOutlet weak var mapView: MKMapView!
     
-    let containerController = OverlayContainerViewController()
+    let containerController = OverlayContainerViewController(style: .flexibleHeight)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        guard let menuVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "") as? 
+        guard let menuVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MapOverlayViewController") as? MapOverlayViewController else { return }
         containerController.delegate = self
-        containerController.viewControllers = []
+        containerController.viewControllers = [menuVC]
     }
 }
 
@@ -31,5 +30,22 @@ extension MapViewController: MKMapViewDelegate {
 }
 
 extension MapViewController: OverlayContainerViewControllerDelegate {
+    enum OverlayNotch: Int, CaseIterable {
+        case minimum, medium, maximum
+    }
     
+    func overlayContainerViewController(_ containerViewController: OverlayContainerViewController, heightForNotchAt index: Int, availableSpace: CGFloat) -> CGFloat {
+        switch OverlayNotch.allCases[index] {
+        case .maximum:
+            return availableSpace * 3 / 4
+        case .medium:
+            return availableSpace / 2
+        case .minimum:
+            return availableSpace * 1 / 4
+        }
+    }
+    
+    func numberOfNotches(in containerViewController: OverlayContainerViewController) -> Int {
+        OverlayNotch.allCases.count
+    }
 }
