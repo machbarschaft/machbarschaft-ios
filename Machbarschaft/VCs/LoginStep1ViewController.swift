0a61e67d45d7b12dfa88ddafbf3c2c291a65d05f
Linus Geffarth <linus@geffarth.de>
2020-03-22 13:45:23 +0100

    set initial storyboard

--- Machbarschaft/VCs/LoginStep1ViewController.swift
+++ Machbarschaft/VCs/LoginStep1ViewController.swift
@@ -17,6 +17,56 @@ class LoginStep1ViewController: UIViewController {
 }
 
 class LoginStep1PageViewController: UIPageViewController {
+    fileprivate lazy var pages: [UIViewController] = {
+        return [
+            self.getViewController(withIdentifier: "Page1"),
+            self.getViewController(withIdentifier: "Page2"),
+            self.getViewController(withIdentifier: "Page3")
+        ]
+    }()
     
+    fileprivate func getViewController(withIdentifier identifier: String) -> UIViewController {
+        return UIStoryboard(name: "Login", bundle: nil).instantiateViewController(withIdentifier: identifier)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.dataSource = self
+        
+        if let firstVC = pages.first {
+            setViewControllers([firstVC], direction: .forward, animated: true, completion: nil)
+        }
+    }
+}
+
+extension LoginStep1PageViewController: UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = pages.firstIndex(of: viewController) else { return nil }
+        let previousIndex = viewControllerIndex - 1
+        guard previousIndex >= 0          else { return pages.last }
+        guard pages.count > previousIndex else { return nil }
+        
+        return pages[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = pages.firstIndex(of: viewController) else { return nil }
+        let nextIndex = viewControllerIndex + 1
+        guard nextIndex < pages.count else { return pages.first }
+        guard pages.count > nextIndex else { return nil }
+        
+        return pages[nextIndex]
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        0
+    }
+    
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        pages.count
+    }
+}
+
+fileprivate class PageViewController: UIViewController {
     
 }
