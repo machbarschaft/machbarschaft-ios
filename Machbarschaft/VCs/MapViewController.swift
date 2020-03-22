bcf3a8fe216adfaf840cc43f71ee07b6feeaeeef
Linus Geffarth <linus@geffarth.de>
2020-03-22 14:12:01 +0100

    hooked up map view

--- Machbarschaft/VCs/MapViewController.swift
+++ Machbarschaft/VCs/MapViewController.swift
@@ -7,11 +7,18 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
-
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+}
+
+extension MapViewController: MKMapViewDelegate {
+    
 }
