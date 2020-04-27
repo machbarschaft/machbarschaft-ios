0212a189076293a9a9d5564abe34560aa8d52a7a
Vishnu Ravi <vishnu.ravi@gmail.com>
2020-04-26 22:39:14 -0400

    Center maps on Job location

--- Machbarschaft/View Controllers/Job Details/JobStep3ViewController.swift
+++ Machbarschaft/View Controllers/Job Details/JobStep3ViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import MapKit
 
 class JobStep3ViewController: UIViewController {
     
     @IBOutlet weak var jobSummaryLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
+    
+    @IBOutlet weak var mapView: MKMapView!
     
     var job: Job!
     
@@ -19,6 +22,11 @@ class JobStep3ViewController: UIViewController {
         super.viewDidLoad()
                 
         jobSummaryLabel.text = "\(job.type.title) für \(job.clientName)"
+        
+        mapView.clipsToBounds = true
+        if let location = job.location {
+            mapView.centerToLocation(location)
+        }
     }
     
     @IBAction func getRoute(_ sender: Any) {
@@ -32,4 +40,17 @@ class JobStep3ViewController: UIViewController {
     @IBAction func completeJob(_ sender: Any) {
         performSegue(withIdentifier: "JobStep3_to_JobCompleted", sender: nil)
     }
+}
+
+private extension MKMapView {
+  func centerToLocation(
+    _ location: CLLocationCoordinate2D,
+    regionRadius: CLLocationDistance = 10000
+  ) {
+    let coordinateRegion = MKCoordinateRegion(
+      center: location,
+      latitudinalMeters: regionRadius,
+      longitudinalMeters: regionRadius)
+    setRegion(coordinateRegion, animated: true)
+  }
 }
